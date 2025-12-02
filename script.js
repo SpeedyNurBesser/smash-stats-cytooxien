2fe10d983ad20b634e7c5db0a0bc98fb15ebe2c8
SpeedyNurBesser <ole.hayn58@gmail.com>
2025-12-02 10:34:02 +0100

    V.1.1.0 Commit - shows player head - indicates rank of player - added kill/death ratio, kill/game ratio, winrate - changed UI design as to see current timeframe better - website opens up using last searched player (localStorage)

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ year.innerText = new Date().getFullYear();
 
 const playername_display = document.getElementById("playername-display");
 
+const playerhead_display = document.getElementById("playerhead-display");
+
 const search_input = document.getElementById("searchInput");
 search_input.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-        show_stats();
+        show_stats(search_input.value, true);
     }
 });
 
@@ -20,6 +22,10 @@ const points_display = document.getElementById("points-display");
 const kills_display = document.getElementById("kills-display");
 const wins_display = document.getElementById("wins-display");
 
+const kill_death_ratio_display = document.getElementById("kill-death-display");
+const kill_game_ratio_display = document.getElementById("kill-game-display");
+const winrate_display = document.getElementById("winrate-display");
+
 const deaths_display = document.getElementById("deaths-display");
 const games_display = document.getElementById("games-display");
 
@@ -28,32 +34,36 @@ const time_display = document.getElementById("time-display");
 const timeframe_button_global = document.getElementById("timeframe-button-global");
 timeframe_button_global.addEventListener("click", () => {
     current_timeframe = "global";
-    show_stats();
+    setActiveTimeframeButton();
+    show_stats(search_input.value, false);
 });
 
 const timeframe_button_month = document.getElementById("timeframe-button-month");
 timeframe_button_month.addEventListener("click", () => {
     current_timeframe = "monthly";
-    show_stats();
+    setActiveTimeframeButton();
+    show_stats(search_input.value, false);
 });
 
 const timeframe_button_week = document.getElementById("timeframe-button-week");
 timeframe_button_week.addEventListener("click", () => {
     current_timeframe = "weekly";
-    show_stats()
+    setActiveTimeframeButton();
+    show_stats(search_input.value, false)
 });
 
 const timeframe_button_day = document.getElementById("timeframe-button-day");
 timeframe_button_day.addEventListener("click", () => {
-    current_timeframe = "daily"
-    show_stats();
+    current_timeframe = "daily";
+    setActiveTimeframeButton();
+    show_stats(search_input.value, false);
 });
 
 let current_timeframe = "global";
 
 document.getElementById("searchButton").addEventListener("click", () => {
     current_timeframe = "global";
-    show_stats();
+    show_stats(search_input.value, true);
 }
 );
 
@@ -75,22 +85,29 @@ async function fetchUserStats(username) {
     return promise;
 }
 
-async function show_stats() {
+async function show_stats(query=search_input.value) {
     // Three cases:
     // 1. User exists -> Show stats
     // 2. User exists but has no stats -> Show message
     // 3. User does not exist -> Show error
 
-    const query = search_input.value;
     fetchUserStats(query)
         .then(data => {
             if (data["status"] == 503) {
                 alert("The API is currently unavailable. Please try again later.");
-                return;
+                return false;
+            }
+
+            if (data["stats"]["sm"]["global"]["games"] == undefined) {
+                alert("User has no stats, i.e. has never played Smash.");
+                return false; 
             }
 
             if (data["stats"]["sm"][current_timeframe]["games"] == undefined) {
-                alert("User has no stats for the selected timeframe.");
+                alert(`No stats available for the selected timeframe (${current_timeframe}). Showing global stats instead.`);
+                current_timeframe = "global";
+                show_stats(query, false);
+                setActiveTimeframeButton();
                 return;
             }
 
@@ -107,12 +124,38 @@ async function show_stats() {
 
             rank_time_display.innerText = `${data["stats"]["sm"][current_timeframe]["rank_time"]}`;
 
+            kill_death_ratio_display.innerText = getKDR(data["stats"]["sm"][current_timeframe]["kills"], data["stats"]["sm"][current_timeframe]["deaths"]);
+            kill_game_ratio_display.innerText = getKGR(data["stats"]["sm"][current_timeframe]["kills"], data["stats"]["sm"][current_timeframe]["games"]);
+            winrate_display.innerText = getWinrate(data["stats"]["sm"][current_timeframe]["wins"], data["stats"]["sm"][current_timeframe]["games"]);
+
             deaths_display.innerText = data["stats"]["sm"][current_timeframe]["deaths"];
             games_display.innerText = data["stats"]["sm"][current_timeframe]["games"];
 
             const playtime_ms = data["stats"]["sm"][current_timeframe]["time"];
             time_display.innerText = "They played for " + millisecondsToHumanReadable(playtime_ms);
+
+            playerhead_display.src = `https://mc-heads.net/avatar/${data["playerInfo"]["username"]}/100/.png`;
+
+            const hex_color_code = data["playerInfo"]["rank"]["color"].replace(/§x§/g, "#").replace(/§/g, "");
+            playername_display.style.color = hex_color_code;
+
+            localStorage.setItem("last_player", query);
         });
+}
+
+function getWinrate(wins, games) {
+    if (games === 0) return "0%";
+    return ((wins / games) * 100).toFixed(2) + "%";
+}
+
+function getKDR(kills, deaths) {
+    if (deaths === 0) return `${kills}.0`;
+    return (kills / deaths).toFixed(2);
+}
+
+function getKGR(kills, games) {
+    if (games === 0) return `${kills}.0`;
+    return (kills / games).toFixed(2);
 }
 
 function millisecondsToHumanReadable(ms) {
@@ -141,3 +184,36 @@ function millisecondsToHumanReadable(ms) {
 
     return result.trim();
 }
+
+function setActiveTimeframeButton() {
+    const buttons = [
+        timeframe_button_global,
+        timeframe_button_month,
+        timeframe_button_week,
+        timeframe_button_day
+    ];
+
+    buttons.forEach(button => {
+        button.style.borderColor = "black";
+    });
+
+    switch (current_timeframe) {
+        case "global":
+            timeframe_button_global.style.borderColor = "#8b8b8b";
+            break;
+        case "monthly":
+            timeframe_button_month.style.borderColor = "#8b8b8b";
+            break;
+        case "weekly":
+            timeframe_button_week.style.borderColor = "#8b8b8b";
+            break;
+        case "daily":
+            timeframe_button_day.style.borderColor = "#8b8b8b";
+            break;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    search_input.value = localStorage.getItem("last_player") || "SpeedyOnlyBetter";
+    show_stats();
+});
